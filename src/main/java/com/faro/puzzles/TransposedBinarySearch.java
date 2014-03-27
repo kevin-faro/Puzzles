882349df59c8b2d84cf50fda0bf2475a73b76451
Kevin Faro <kevin.faro@gmail.com>
2014-03-27 19:17:39 -0400

    simplified transposed binary search

--- src/main/java/com/faro/puzzles/TransposedBinarySearch.java
+++ src/main/java/com/faro/puzzles/TransposedBinarySearch.java
@@ -2,53 +2,45 @@ package com.faro.puzzles;
 
 public class TransposedBinarySearch {
 
-	// Given a sorted list that has been transposed
-	// that is, a portion has been removed from one end and attached to the
-	// other
-	// write a function to determine if a given number is present in the list.
-	public static boolean isInList(float targetValue, float[] list) {
-		if (list == null || list.length == 0) {
-			return false;
-		}
-		return inRange(targetValue, list, 0, list.length - 1);
-	}
+    // Given a sorted list that has been transposed
+    // that is, a portion has been removed from one end and attached to the
+    // other
+    // write a function to determine if a given number is present in the list.
+    public static boolean isInList(float targetValue, float[] list) {
+        if (list == null || list.length == 0) {
+            return false;
+        }
+        return inRange(targetValue, list, 0, list.length - 1);
+    }
 
-	private static boolean inRange(float targetValue, float[] list, int start,
-			int end) {
-		if (start > end) {
-			return false;
-		} else if (start == end) {
-			return targetValue == list[start];
-		}
+    private static boolean inRange(float targetValue, float[] list, int start, int end) {
+        if (start > end) {
+            return false;
+        }
 
-		int midIndex = (int) Math.ceil((end - start) / 2.0) + start;
-		float mid = list[midIndex];
+        int midIndex = start + (end - start) / 2;
 
-		if (targetValue == mid) {
-			return true;
-		} else {
-			if (list[start] > list[end]) {
-				// transposed
-				if (targetValue > mid) {
-					if (targetValue <= list[end]) {
-						return inRange(targetValue, list, midIndex + 1, end);
-					} else {
-						return inRange(targetValue, list, start, midIndex - 1);
-					}
-				} else {
-					if (targetValue >= list[start]) {
-						return inRange(targetValue, list, start, midIndex - 1);
-					} else {
-						return inRange(targetValue, list, midIndex + 1, end);
-					}
-				}
-			} else {
-				if (targetValue > mid) {
-					return inRange(targetValue, list, midIndex + 1, end);
-				} else {
-					return inRange(targetValue, list, start, midIndex - 1);
-				}
-			}
-		}
-	}
+        if (targetValue == list[midIndex]) {
+            return true;
+        }
+
+        if (isTransposed(targetValue, list, start, end)) {
+            // transposed
+            if (targetValue > list[end]) {
+                return inRange(targetValue, list, start, midIndex - 1);
+            } else {
+                return inRange(targetValue, list, midIndex + 1, end);
+            }
+        } else {
+            if (targetValue > list[midIndex]) {
+                return inRange(targetValue, list, midIndex + 1, end);
+            } else {
+                return inRange(targetValue, list, start, midIndex - 1);
+            }
+        }
+    }
+
+    private static boolean isTransposed(float targetValue, float[] list, int start, int end) {
+        return list[start] > list[end];
+    }
 }
